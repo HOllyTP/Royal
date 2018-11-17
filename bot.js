9603c56dd2595d999ad960551c46741944ffb49e
HOllyTP <45126905+HOllyTP@users.noreply.github.com>
2018-11-17 17:35:09 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(Logged in as ${client.user.tag}!);
+});
+
+
+client.on('ready', async() => {
+var server = "Id server"; // 510399878002901002
+var channel = "id room";//510399878002901005
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , ')
+    },305);
+})
+
+client.login("513390569389686814.kBr_0CaiuYCIzP4UbwzZ9LqHNmw");
